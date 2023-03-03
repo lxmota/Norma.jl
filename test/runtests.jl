298c17ac64d4d1cc78735fced7b18652ccc2da15
Alejandro Mota <amota@sandia.gov>
2023-03-03 14:16:10 -0800

    Split tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,14 +4,12 @@ using Test
 include("../src/norma.jl")
 include("helpers.jl")
 
-@testset "Norma" begin
-    include("interpolation.jl")
-    include("single-static-solid-cube.jl")
-    include("single-static-solid-neumann-bc.jl")
-    include("single-implicit-dynamic-solid-cube.jl")
-    include("single-implicit-dynamic-solid-sho.jl")
-    include("single-implicit-dynamic-solid-clamped.jl")
-    include("single-explicit-dynamic-solid-cube.jl")
-    include("single-explicit-dynamic-solid-sho.jl")
-    include("single-explicit-dynamic-solid-clamped.jl")
-end
+include("interpolation.jl")
+include("single-static-solid-cube.jl")
+include("single-static-solid-neumann-bc.jl")
+include("single-implicit-dynamic-solid-cube.jl")
+include("single-implicit-dynamic-solid-sho.jl")
+include("single-implicit-dynamic-solid-clamped.jl")
+include("single-explicit-dynamic-solid-cube.jl")
+include("single-explicit-dynamic-solid-sho.jl")
+include("single-explicit-dynamic-solid-clamped.jl")
