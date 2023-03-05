683146bb6949e02702b029ceef43a0df8aa3418e
Alejandro Mota <amota@sandia.gov>
2023-03-04 18:05:26 -0800

    Tighten tolerances clamped prep Schwarz

--- test/single-explicit-dynamic-solid-clamped.jl
+++ test/single-explicit-dynamic-solid-clamped.jl
@@ -14,20 +14,20 @@
     min_acce = minimum_components(integrator.acceleration)
     @test max_disp[1] ≈ 0.0 atol = 1.0e-06
     @test max_disp[2] ≈ 0.0 atol = 1.0e-06
-    @test max_disp[3] ≈ 0.00882497 rtol = 1.0e-04
+    @test max_disp[3] ≈ 0.00882497 rtol = 8.0e-05
     @test max_velo[1] ≈ 0.0 atol = 1.0e-06
     @test max_velo[2] ≈ 0.0 atol = 1.0e-06
-    @test max_velo[3] ≈ 98.7857 rtol = 1.0e-03
+    @test max_velo[3] ≈ 98.7781 rtol = 6.0e-04
     @test max_acce[1] ≈ 0.0 atol = 1.0e-06
     @test max_acce[2] ≈ 0.0 atol = 1.0e-06
-    @test max_acce[3] ≈ 7.95633e6 rtol = 1.0e-03
+    @test max_acce[3] ≈ 7.95606e6 rtol = 6.0e-04
     @test min_disp[1] ≈ 0.0 atol = 1.0e-06
     @test min_disp[2] ≈ 0.0 atol = 1.0e-06
     @test min_disp[3] ≈ 0.0 atol = 1.0e-06
     @test min_velo[1] ≈ 0.0 atol = 1.0e-06
     @test min_velo[2] ≈ 0.0 atol = 1.0e-06
-    @test min_velo[3] ≈ -220.624 rtol = 1.0e-03
+    @test min_velo[3] ≈ -220.624 rtol = 5.0e-04
     @test min_acce[1] ≈ 0.0 atol = 1.0e-06
     @test min_acce[2] ≈ 0.0 atol = 1.0e-06
-    @test min_acce[3] ≈ -1.65468e7 rtol = 1.0e-05
+    @test min_acce[3] ≈ -1.65468e7 rtol = 2.0e-06
 end
