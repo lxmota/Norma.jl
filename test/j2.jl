47e4be537349d6b5a127e45b14f9725fb805ad38
Mota <amota@s1094950ca.ca.sandia.gov>
2023-04-07 19:41:09 -0700

    Add driver for separate j2 test
    Not part of the main tests for now until
    we sort out how we will deal with a more
    general class of material models.

---
+++ test/j2.jl
@@ -0,0 +1,16 @@
+include("../src/constitutive_def.jl")
+include("../src/constitutive.jl")
+
+using LinearAlgebra
+
+params = Dict{Any,Any}()
+params["elastic modulus"] = 200.0e+09
+params["Poisson's ratio"] = 0.25
+params["density"] = 7800.0
+params["yield stress"] = 1.0e+09
+material = J2(params)
+F = [1.01 0.0 0.0; 0.0 1.0 0.0; 0.0 0.0 1.0]
+Fp = [1.0 0.0 0.0; 0.0 1.0 0.0; 0.0 0.0 1.0]
+eqps = 0.0
+dt = 1.0e-06
+Fe, Fp, eqps, sigma = stress_update(material, F, Fp, eqps, dt)
