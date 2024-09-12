8676ae4137550a853ae58823f9f1f3de1492e172
Alejandro Mota <amota@sandia.gov>
2024-09-12 16:44:52 -0700

    Add super nonlinear energy WIP

--- src/minitensor.jl
+++ src/minitensor.jl
@@ -35,6 +35,40 @@ function symm(A::AbstractMatrix{Float64})
     return 0.5 * (A + A')
 end
 
+function determinant(A::AbstractMatrix{Float64})
+    return -A[1,3]*A[2,2]*A[3,1] + A[1,2]*A[2,3]*A[3,1] +
+            A[1,3]*A[2,1]*A[3,2] - A[1,1]*A[2,3]*A[3,2] -
+            A[1,2]*A[2,1]*A[3,3] + A[1,1]*A[2,2]*A[3,3];
+end
+
+function trace(A::AbstractMatrix{Float64})
+    return A[1,1] + A[2,2] + A[3,3];
+end
+
+function transpose(A::AbstractMatrix{Float64})
+    return [
+        A[1,1] A[2,1] A[3,1]
+        A[1,2] A[2,2] A[3,2]
+        A[1,3] A[2,3] A[3,3]
+    ]
+end
+
+function dot(A::AbstractMatrix{Float64}, B::AbstractMatrix{Float64})
+    C = zeros(3, 3)
+    C[1, 1] = A[1, 1] * B[1, 1] + A[1, 2] * B[2, 1] + A[1, 3] * B[3, 1];
+    C[1, 2] = A[1, 1] * B[1, 2] + A[1, 2] * B[2, 2] + A[1, 3] * B[3, 2];
+    C[1, 3] = A[1, 1] * B[1, 3] + A[1, 2] * B[2, 3] + A[1, 3] * B[3, 3];
+
+    C[2, 1] = A[2, 1] * B[1, 1] + A[2, 2] * B[2, 1] + A[2, 3] * B[3, 1];
+    C[2, 2] = A[2, 1] * B[1, 2] + A[2, 2] * B[2, 2] + A[2, 3] * B[3, 2];
+    C[2, 3] = A[2, 1] * B[1, 3] + A[2, 2] * B[2, 3] + A[2, 3] * B[3, 3];
+
+    C[3, 1] = A[3, 1] * B[1, 1] + A[3, 2] * B[2, 1] + A[3, 3] * B[3, 1];
+    C[3, 2] = A[3, 1] * B[1, 2] + A[3, 2] * B[2, 2] + A[3, 3] * B[3, 2];
+    C[3, 3] = A[3, 1] * B[1, 3] + A[3, 2] * B[2, 3] + A[3, 3] * B[3, 3];
+    return C
+end
+
 #
 # Baker-Campbell-Hausdorff formula, up to 8 terms.
 #
